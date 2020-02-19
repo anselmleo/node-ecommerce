ddf8e347a222c55b03a62dc01139c66d97326f7f
Anselm <anselmleo@gmail.com>
2020-02-19 11:54:55 +0100

    apply express validator to create user route

--- routes/users.js
+++ routes/users.js
@@ -2,6 +2,8 @@ var express = require("express");
 var router = express.Router();
 const userController = require('../controllers/userController');
 const auth = require('../middlewares/auth');
+const { userValidationRules, validate } = require('../validator.js');
+
 
 /* GET users listing. */
 router.get("/user", function(req, res, next) {
@@ -9,8 +11,16 @@ router.get("/user", function(req, res, next) {
 });
 
 /* POST new user */
-router.post('', userController.createUser);
+router.post('', userValidationRules(), validate, userController.createUser);
 router.get('/:id', auth, userController.getOneUser);
 router.get('', auth, userController.getAllUsers);
 
 module.exports = router;
+
+
+// app.post('/user', userValidationRules(), validate, (req, res) => {
+//   User.create({
+//     username: req.body.username,
+//     password: req.body.password,
+//   }).then(user => res.json(user))
+// })
---
+++ validator.js
@@ -0,0 +1,29 @@
+const { body, validationResult } = require("express-validator");
+
+const userValidationRules = () => {
+  return [
+    body('firstname').isString().isLength({min: 2}),
+    body('lastname').isString().isLength({min: 2}),
+    body('email').isEmail(),
+    body('password').isLength({ min: 4 })
+  ]
+}
+
+const validate = (req, res, next) => {
+  const errors = validationResult(req);
+  if (errors.isEmpty()) {
+    return next()
+  }
+
+  const extractedErrors = [];
+  errors.array().map(err => extractedErrors.push({ [err.param]: err.msg }));
+
+  return res.status(422).json({
+    errors: extractedErrors,
+  });
+}
+
+module.exports = {
+  validate,
+  userValidationRules
+}
