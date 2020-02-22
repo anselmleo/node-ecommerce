59835b330a901a8b7819e52458824de0b4efa17d
Anselm <anselmleo@gmail.com>
2020-02-22 12:13:23 +0100

    setup default route

--- routes/index.js
+++ routes/index.js
@@ -2,19 +2,8 @@ var express = require("express");
 var router = express.Router();
 
 /* GET home page. */
-router.get("/", checkName, function(req, res, next) {
+router.get("/", function(req, res, next) {
   res.status(200).send({ title: "Express" });
 });
-
-function checkName(req, res, next) {
-  data = req.params.name;
-  if (data == "tolu") {
-    return res.status(400).send({
-      error: "name not correct"
-    });
-  }
-
-  next();
-}
 
 module.exports = router;
